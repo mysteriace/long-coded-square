d00b684999b59fcdac1fa51d8d68fce6de01ec71
Tastio <github@codedsoup.com>
2016-05-12 19:31:16 -0700

    Added a small information bar.

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+    print "<p>Welcome to Square.</p>";
+    print "<p>Square has mini webpages for the developers of Longcoded.</p>";
+    print "<p>Ragweed and Tastie are currently approved.</p>";
+
+?>
