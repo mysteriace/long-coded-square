6ffd9d5f8ee197f4f23cb21c57b868ccc6516b46
Tastio <github@codedsoup.com>
2016-05-13 16:27:38 -0700

    Added awesome fal.

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 
     print "<p>Welcome to Square.</p>";
     print "<p>Square has mini webpages for the developers of Longcoded.</p>";
-    print "<p>Ragweed and Tastie are currently approved.</p>";
+    print "<p>Ragweed, Tastie, and falceso are currently approved.</p>";
 
 ?>
